6e3c7ca75b89c6bbe4e6b00d3b7394e403fb3246
lucasriccioppo <lucasriccioppo@hotmail.com>
2020-03-13 21:52:54 -0300

    first commit here.

---
+++ main.py
@@ -0,0 +1,18 @@
+# hypercorn uvicorn main:app --reload
+# docker run --name t10 -e POSTGRES_PASSWORD=123 -d -p 5432:5432 postgres
+# docker exec -it t10 bash
+# psql -U postgres
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
